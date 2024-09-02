03e8c0a2e699dc9889ba7bf65f5393a3184cea04
Zoran Pesic <zoran1991@gmail.com>
2024-09-02 04:15:30 -0700

    some basic styling

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user_name = current_user.fitbit_client.profile[:user][:display_name]
+    @user_profile = current_user.fitbit_client.profile[:user]
     @devices = current_user.fitbit_client.devices
     @daily_summary = current_user.fitbit_client.daily_activity_summary
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def time_based_greeting(time_zone)
+    current_hour = Time.current.in_time_zone(time_zone).hour
+
+    if current_hour.between?(5, 11)
+      "Good morning"
+    elsif current_hour.between?(12, 17)
+      "Good afternoon"
+    else
+      "Good evening"
+    end
+  end
 end
